5e2b0b0f1cd55311d3a92141612bfe1c22241a36
Sébastien Lau <sebastien.lau2021@gmail.com>
2026-01-17 13:08:02 +0100

    add search input component

---
+++ scripts/components/searchInput.js
@@ -0,0 +1,99 @@
+import { crossSmall, searchSm } from "../constants";
+
+const SearchInputMap = new Map();
+
+const defaultProps = {
+  placeholder: "Rechercher une recette, un ingrédient ...",
+  type: "text",
+  name: "search",
+  id: "search",
+};
+
+function SearchInput({ props = {} || defaultProps }) {
+  const inputContainer = document.createElement("div");
+  inputContainer.setAttribute("class", "search-input-base input-base");
+
+  const input = document.createElement("input");
+
+  // spread input props
+  const mergedProps = { ...defaultProps, ...props };
+  Object.entries(mergedProps).forEach(([k, v]) => {
+    input.setAttribute(k, v);
+  });
+  input.setAttribute("class", "input-field text-body1");
+
+  // create end adornment
+  const endAdornment = document.createElement("div");
+  endAdornment.setAttribute("class", "end-adornment");
+  // create clear button
+  const clearButton = document.createElement("button");
+  clearButton.setAttribute(
+    "class",
+    "button-base icon-button sm clear-button hidden",
+  );
+  clearButton.innerHTML = crossSmall;
+  endAdornment.appendChild(clearButton);
+  //create search button
+  const searchButton = document.createElement("button");
+  searchButton.setAttribute("class", "button-base icon-button sm");
+  searchButton.innerHTML = searchSm;
+  endAdornment.appendChild(searchButton);
+
+  inputContainer.appendChild(input);
+  inputContainer.appendChild(endAdornment);
+
+  handleClearInput(inputContainer);
+
+  return inputContainer;
+}
+
+// set up for present search inputs in the DOM
+const searchInput = document.querySelectorAll(".search-input-base");
+function handleFocusInput() {
+  Array.from(searchInput).forEach((element) => {
+    handleClearInput(element);
+  });
+}
+
+function handleClearInput(element) {
+  const input = element.querySelector("input");
+  const clearButton = element.querySelector(".clear-button");
+
+  const handleClearInput = () => {
+    input.value = "";
+    clearButton.classList.add("hidden");
+    input.dispatchEvent(new CustomEvent("inputCleared"));
+  };
+
+  const inputListeners = {
+    input: () => {
+      if (input.value.length === 0) {
+        clearButton.removeEventListener("click", handleClearInput);
+      } else {
+        clearButton.addEventListener("click", handleClearInput);
+        clearButton.classList.remove("hidden");
+      }
+    },
+    focus: () => {
+      element.classList.add("focused");
+    },
+    blur: () => {
+      element.classList.remove("focused");
+    },
+    removeElement: () => {
+      input.removeEventListener("focus", inputListeners.focus);
+      input.removeEventListener("blur", inputListeners.blur);
+      input.removeEventListener("input", inputListeners.input);
+      clearButton.removeEventListener("click", handleClearInput);
+      SearchInputMap.delete(element);
+      element.remove();
+    },
+  };
+  SearchInputMap.set(element, inputListeners);
+
+  input.addEventListener("focus", inputListeners.focus);
+  input.addEventListener("blur", inputListeners.blur);
+  input.addEventListener("input", inputListeners.input);
+}
+
+export { SearchInput, handleFocusInput, handleClearInput, SearchInputMap };
