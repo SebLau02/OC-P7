caf5df0a1d09ef1f6bc99fbdad4870ed0b351826
Sébastien Lau <sebastien.lau2021@gmail.com>
2026-01-17 16:00:24 +0100

    add: empty result message suggestion

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -2,9 +2,13 @@ import { RecipeCard } from "./components/recipeCard";
 import { getRecipes } from "./fetchData";
 
 const recipesContainer = document.getElementById("result-container");
+const recipeCount = document.getElementById("recipe-count");
+const emptyResult = document.getElementById("empty-result");
 
 async function recipes() {
-  const recipes = await getRecipes();
+  //   const recipes = await getRecipes();
+
+  const recipes = [];
 
   const fragment = document.createDocumentFragment();
   recipes.forEach((recipe) => {
@@ -12,6 +16,16 @@ async function recipes() {
     fragment.appendChild(recipeCard);
   });
   recipesContainer.appendChild(fragment);
+  recipeCount.textContent = `${recipes.length} recettes`;
+  if (recipes.length === 0) {
+    const text = emptyResult.textContent
+      .replace("{{search}}", "tarte au pomme") // à remplacer par le champ de recherche
+      .replace("{{suggest}}", "tarte aux fraises"); // à remplacer par une suggestion pertinente
+    emptyResult.textContent = text;
+    emptyResult.classList.remove("hidden");
+  } else if (emptyResult.classList.contains("hidden") === false) {
+    emptyResult.classList.add("hidden");
+  }
 }
 
 export { recipes };
